dbd6672cd0236094de4d7cc29641495486e83f82
deb <debmalya.biswas25@gmail.com>
2020-08-17 20:01:32 +0530

    Added initial configuration

---
+++ src/main/java/com/deb/employee/attendance/authorization/EmpAuthApplication.java
@@ -0,0 +1,13 @@
+package com.deb.employee.attendance.authorization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deb/employee/attendance/authorization/EmpAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deb.employee.attendance.authorization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
